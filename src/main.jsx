1402c79e7d01504f1b40b6fcabf178332a65d020
zyn-codes <vberryfrost301@gmail.com>
2025-09-26 17:11:52 +0000

    finallll

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { startSubmissionQueue } from './utils/submitQueue'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,3 +12,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Router>
   </React.StrictMode>,
 )
+
+// Start the background queue processor for pending submissions
+try {
+  startSubmissionQueue({ intervalMs: 30000 });
+  // expose for debug in browser console
+  window.__somatic_queue = {
+    queuedCount: () => import('./utils/submitQueue').then(m => m.queuedCount()),
+    clear: () => import('./utils/submitQueue').then(m => m._clearQueue())
+  };
+} catch (e) {
+  // non-fatal
+  console.debug('Failed to start submission queue', e);
+}
